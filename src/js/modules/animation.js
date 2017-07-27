4831b75d64533eba03a651ab2016caf084388d2d
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-07-27 12:02:19 -0400

    adjust timings

--- src/js/modules/animation.js
+++ src/js/modules/animation.js
@@ -2,8 +2,8 @@ var $ = require('../vendor/jquery.js');
 
 var counter = 0;
 var face = 0;
-var timings = [0, 10, 12, 16, 18, 20, 26, 34, 36, 40, 52, 53];
-var frames =  [0, 0, 1, 2, 3,  1,  6,  5,  4,  5,  2,  0]
+var timings = [0, 10, 12, 16, 18, 20, 26, 34, 36, 51, 52, 53];
+var frames =  [0, 0,  1,  2,  3,   1,  6,  5,  4,  5,  2,  0]
 
 module.exports = {
     init: function() {
@@ -11,7 +11,7 @@ module.exports = {
     },
 
     animate: function() {
-        var interval = setInterval( this.toggleFaces, 100 );
+        var interval = setInterval( this.toggleFaces, 200 );
     },
 
     toggleFaces: function() {
